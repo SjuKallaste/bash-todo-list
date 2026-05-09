c1abbcd65875312d4105e3d0eee82d9f9db495e8
lively <mullerenjoyer@gmail.com>
2026-05-09 14:46:22 +0300

    added the ability to edit tasks

--- todo list.sh
+++ todo list.sh
@@ -71,13 +71,74 @@ view_tasks() {
     for line in "${lines[@]}"; do
         [[ -z "$line" ]] && continue
         local id title status date notes
-        id=$(echo "$line" | cut -d',' -f1)
-        title=$(echo "$line" | cut -d',' -f2)
+        id=$(    echo "$line" | cut -d',' -f1)
+        title=$( echo "$line" | cut -d',' -f2)
         status=$(echo "$line" | cut -d',' -f3)
-        date=$(echo "$line" | cut -d',' -f4)
-        notes=$(echo "$line" | cut -d',' -f5-)
+        date=$(  echo "$line" | cut -d',' -f4)
+        notes=$( echo "$line" | cut -d',' -f5-)
         printf "%-4s  %-28s  %-10s  %-12s  %s\n" "$id" "$title" "$status" "$date" "$notes"
     done
+}
+
+edit_task() {
+    view_tasks
+
+    printf "Task ID to edit: "
+    read -r target_id
+
+    if [[ ! "$target_id" =~ ^[0-9]+$ ]]; then
+        printf "Error: ID must be a number.\n"
+        return 1
+    fi
+
+    if ! id_exists "$target_id"; then
+        printf "Error: no task with ID %s.\n" "$target_id"
+        return 1
+    fi
+
+    local current_line cur_title cur_status cur_date cur_notes
+    current_line=$(grep "^${target_id}," "$CSV_FILE")
+    cur_title=$( echo "$current_line" | cut -d',' -f2)
+    cur_status=$(echo "$current_line" | cut -d',' -f3)
+    cur_date=$(  echo "$current_line" | cut -d',' -f4)
+    cur_notes=$( echo "$current_line" | cut -d',' -f5-)
+
+    printf "Leave blank to keep the current value.\n"
+
+    local new_title new_date new_notes
+    printf "New title [%s]: " "$cur_title"
+    read -r new_title
+    [[ -z "${new_title// }" ]] && new_title="$cur_title"
+    new_title="${new_title//,/;}"
+
+    printf "New date [%s] (YYYY-MM-DD): " "$cur_date"
+    read -r new_date
+    if [[ -z "${new_date// }" ]]; then
+        new_date="$cur_date"
+    elif [[ ! "$new_date" =~ ^[0-9]{4}-[0-1][0-9]-[0-3][0-9]$ ]]; then
+        printf "Error: invalid date format. Keeping current date.\n"
+        new_date="$cur_date"
+    fi
+
+    printf "New notes [%s]: " "$cur_notes"
+    read -r new_notes
+    [[ -z "${new_notes// }" ]] && new_notes="$cur_notes"
+    new_notes="${new_notes//,/;}"
+
+    local new_line tmp
+    new_line="${target_id},${new_title},${cur_status},${new_date},${new_notes}"
+    tmp=$(mktemp)
+
+    while IFS= read -r row; do
+        if [[ "$row" == ${target_id},* ]]; then
+            printf "%s\n" "$new_line"
+        else
+            printf "%s\n" "$row"
+        fi
+    done < "$CSV_FILE" > "$tmp"
+    mv "$tmp" "$CSV_FILE"
+
+    printf "Task #%s updated.\n" "$target_id"
 }
 
 delete_task() {
@@ -148,25 +209,27 @@ mark_done() {
     printf "Task #%s marked as done.\n" "$target_id"
 }
 
-    while true; do
-        printf "\n"
-        printf "1) Add Task\n"
-        printf "2) View Tasks\n"
-        printf "3) Delete Task\n"
-        printf "4) Mark as Done\n"
-        printf "0) Quit\n"
-        printf "Choice: "
-        read -r choice
+while true; do
+    printf "\n"
+    printf "1) Add Task\n"
+    printf "2) View Tasks\n"
+    printf "3) Edit Task\n"
+    printf "4) Delete Task\n"
+    printf "5) Mark as Done\n"
+    printf "0) Quit\n"
+    printf "Choice: "
+    read -r choice
 
-        case "$choice" in
-            1) add_task    ;;
-            2) view_tasks  ;;
-            3) delete_task ;;
-            4) mark_done   ;;
-            0) printf "Goodbye.\n"; exit 0 ;;
-            *) printf "Invalid option.\n" ;;
-        esac
+    case "$choice" in
+        1) add_task    ;;
+        2) view_tasks  ;;
+        3) edit_task   ;;
+        4) delete_task ;;
+        5) mark_done   ;;
+        0) printf "Goodbye.\n"; exit 0 ;;
+        *) printf "Invalid option.\n" ;;
+    esac
 
-        printf "\nPress Enter to continue..."
-        read -r _
-    done
+    printf "\nPress Enter to continue..."
+    read -r _
+done
